249fd033934442f1f497a522713edf867ea3e58b
DaviCardosoF <cardosodaviferreira@gmail.com>
2024-11-27 03:45:31 -0300

    atualização room

--- app/src/main/java/br/edu/up/rgm33597782/data/Item.kt
+++ app/src/main/java/br/edu/up/rgm33597782/data/Item.kt
@@ -16,11 +16,15 @@
 
 package br.edu.up.rgm33597782.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 /**
  * Entity data class represents a single row in the database.
  */
+@Entity(tableName = "items")
 class Item(
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val name: String,
     val price: Double,
